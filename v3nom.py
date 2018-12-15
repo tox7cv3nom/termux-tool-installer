d13635294dcf577efba7ba7735dadd16354daa77
toxic v3nom <nhibtaungamain@gmail.com>
2018-12-15 10:33:30 +0530

    Update v3nom.py

--- v3nom.py
+++ v3nom.py
@@ -19,7 +19,7 @@ def animation():
 """)
  
 def install():
-  print '#####thanx for using my tool######'
+  print ('#####thanx for using my tool######')
   print 'please select one tool'
   print '1.bingo  ~dork exploiter'
   print '2.beelogger ~python keylogger'
